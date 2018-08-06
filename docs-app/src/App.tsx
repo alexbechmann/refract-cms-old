53c666c0f09c8689370c80e5ad90764ca5d0b87b
Alex Bechmann <alex.bechmann@danfoss.com>
2018-08-06 16:33:22 +0200

    docs app & build

---
+++ docs-app/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Typography } from '@material-ui/core';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <Typography variant='title'>Firestore-CMS</Typography>
+        <p>Docs coming soon...</p>
+      </div>
+    );
+  }
+}
+
+export default App;
