74dfcf59e0683f2cd4cac94b97103b7c3380b681
Alex Bechmann <alex.bechmann@danfoss.com>
2018-08-06 17:00:03 +0200

    docs path

--- docs-app/src/App.tsx
+++ docs-app/src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Typography } from '@material-ui/core';
 
 class App extends React.Component {
-  public render() {
+  render() {
     return (
       <div className="App">
         <Typography variant='title'>Firestore-CMS</Typography>
