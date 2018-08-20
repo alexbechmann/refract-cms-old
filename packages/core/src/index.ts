d47e3afbc7c03f19eecb1d154958a828f8654360
Alex Bechmann <alex.bechmann@outlook.com>
2018-08-20 23:49:14 +0200

    symlinks working by removing ref to folder

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,18 @@
 export { default as configure } from './config/configure';
 export { default as Admin } from './Admin';
 export { default as defineEntity } from './entities/define-entity';
-export { PropertyEditorProps } from './properties/property-editor-props';
-export { default as TextEditor } from './property-editors/TextEditor';
-export { default as EntityPickerEditor } from './property-editors/EntityPickerEditor';
-export { SingleMediaPickerEditor, MultipleMediaPickerEditor } from './property-editors/MediaPickerEditor';
-export { default as LocationEditor } from './property-editors/LocationEditor';
-export { default as SingleDropdownEditor } from './property-editors/SingleDropdownEditor';
-export { default as MultipleDropdownEditor } from './property-editors/MultipleDropdownEditor';
-export { default as DatePickerEditor } from './property-editors/DatePickerEditor';
 export { Entity } from './entities/entity.model';
 export { Location } from './location/location.model';
 export { default as entityService } from './entities/entity.service';
 export { default as mediaService } from './media/media.service';
 export { MediaItem } from './media/media-item.model';
+
+// Property Editors
+export { PropertyEditorProps } from './properties/property-editor-props';
+export { SingleMediaPickerEditor, MultipleMediaPickerEditor } from './property-editors/MediaPickerEditor';
+export { default as TextEditor } from './property-editors/TextEditor';
+export { default as EntityPickerEditor } from './property-editors/EntityPickerEditor';
+export { default as LocationEditor } from './property-editors/LocationEditor';
+export { default as SingleDropdownEditor } from './property-editors/SingleDropdownEditor';
+export { default as MultipleDropdownEditor } from './property-editors/MultipleDropdownEditor';
+export { default as DatePickerEditor } from './property-editors/DatePickerEditor';
