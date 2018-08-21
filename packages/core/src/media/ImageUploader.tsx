0b1476cb3cf9e44baee71bf2981fe84978aef6ec
Alex Bechmann <alex.bechmann@outlook.com>
2018-08-21 22:58:26 +0200

    image dialog

--- packages/core/src/media/ImageUploader.tsx
+++ packages/core/src/media/ImageUploader.tsx
@@ -1,127 +1,84 @@
 import * as React from 'react';
 import * as ReactCrop from 'react-image-crop';
-import { LinearProgress, IconButton, Button, CircularProgress } from '@material-ui/core';
+import {
+  LinearProgress,
+  IconButton,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  DialogTitle,
+  DialogContentText
+} from '@material-ui/core';
 import * as Icons from '@material-ui/icons';
 import * as createUniqueString from 'unique-string';
 import mediaService from './media.service';
 
 interface State {
-  // cropperSrc?: string;
-  // crop?: any;
-  // selectedFile?: File;
-  // croppedBlob?: Blob;
-  // croppedPreviewSrc?: string;
   file?: File;
   uploading: boolean;
+  dialogOpen: boolean;
 }
 
-interface ImageUploaderProps {
+export interface ImageUploaderProps {
   onUploaded?: () => void;
 }
 
 class ImageUploader extends React.Component<ImageUploaderProps, State> {
   state: State = {
-    uploading: false
+    uploading: false,
+    dialogOpen: false
   };
 
   constructor(props) {
     super(props);
     this.upload = this.upload.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+  }
+
+  toggleOpen() {
+    this.setState(state => ({
+      dialogOpen: !state.dialogOpen
+    }));
   }
 
   render() {
     return (
       <div>
-        {/* {this.state.uploadSnapshot && this.renderProgress()} */}
-        {/* {this.state.cropperSrc && this.renderCropper()} */}
-        <input hidden onChange={this.handleImageChange} accept="image/*" id="icon-button-file" type="file" />
-        <label htmlFor="icon-button-file">
-          <IconButton color="primary" component="span">
-            <Icons.Photo />
-          </IconButton>
-        </label>
-        {/* {this.state.croppedBlob && this.renderPreview()} */}
-        {this.state.file && !this.state.uploading ? <Button onClick={this.upload}>Upload</Button> : <React.Fragment />}
-        {this.state.uploading && <CircularProgress />}
+        <Button onClick={this.toggleOpen}>Upload image</Button>
+        <Dialog open={this.state.dialogOpen}>
+          <DialogTitle>Upload an image</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Choose an image & click upload.</DialogContentText>
+            {this.state.uploading && <LinearProgress />}
+            <input hidden onChange={this.handleImageChange} accept="image/*" id="icon-button-file" type="file" />
+            <label htmlFor="icon-button-file">
+              <Button color="primary" component="span">
+                <Icons.Photo />
+                Select photo
+              </Button>
+            </label>
+          </DialogContent>
+          <DialogActions>
+            {this.state.file && !this.state.uploading ? (
+              <Button onClick={this.upload}>Upload</Button>
+            ) : (
+              <React.Fragment />
+            )}
+            <Button onClick={this.toggleOpen}>Done</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
-
-  // renderCropper() {
-  //   return (
-  //     <ReactCrop
-  //       src={this.state.cropperSrc}
-  //       onChange={crop => this.setState({ crop })}
-  //       onImageLoaded={image => {
-  //         const crop = { x: 0, y: 0, width: image.width, height: image.height };
-  //         this.setState({ crop });
-  //         this.handleCropComplete(null, crop);
-  //       }}
-  //       crop={this.state.crop}
-  //       style={{
-  //         height: '100px'
-  //       }}
-  //       onComplete={this.handleCropComplete}
-  //     />
-  //   );
-  // }
-
-  // renderPreview() {
-  //   return (
-  //     <div>
-  //       <img style={{ maxHeight: 100, maxWidth: 100 }} src={this.state.croppedPreviewSrc} />
-  //       <Button onClick={this.upload}> Upload</Button>
-  //     </div>
-  //   );
-  // }
-
-  // handleCropComplete = (_, crop) => {
-  //   const canvas = document.createElement('canvas');
-  //   canvas.width = crop.width;
-  //   canvas.height = crop.height;
-  //   const ctx = canvas.getContext('2d');
-
-  //   const image = new Image();
-  //   image.src = this.state.cropperSrc;
-  //   ctx.drawImage(image, crop.x, crop.y, crop.width, crop.height, 0, 0, crop.width, crop.height);
-
-  //   // As Base64 string
-  //   // const base64Image = canvas.toDataURL('image/jpeg');
-
-  //   // As a blob
-  //   canvas.toBlob(croppedBlob => {
-  //     //file.name = fileName;
-  //     this.setState({
-  //       croppedBlob
-  //     });
-  //     this.toDataUrl(croppedBlob).then(dataUrl => {
-  //       this.setState({
-  //         croppedPreviewSrc: dataUrl
-  //       });
-  //     });
-  //   }, 'image/jpeg');
-  // };
 
   handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files[0];
     this.setState({
       file
     });
-    // this.toDataUrl(selectedFile).then(dataUrl => {
-    //   this.setState({
-    //     cropperSrc: dataUrl,
-    //     selectedFile
-    //   });
-    // });
   };
-
-  // toDataUrl(file) {
-  //   return new Promise<string>((resolve, reject) => {
-  //     var fileReader = new FileReader();
-  //     fileReader.onload = e => resolve(e.target.result);
-  //     fileReader.readAsDataURL(file);
-  //   });
-  // }
 
   upload() {
     const uniqueString = createUniqueString();
@@ -135,34 +92,7 @@ class ImageUploader extends React.Component<ImageUploaderProps, State> {
         this.props.onUploaded();
       }
     });
-
-    // const imageRef = firebase
-    //   .storage()
-    //   .ref()
-    //   .child('refract-cms')
-    //   .child('media')
-    //   .child(`${uniqueString}_${this.state.selectedFile.name}`);
-    // const uploadTask = imageRef.put(this.state.croppedBlob);
-    // uploadTask.on(
-    //   'state_changed',
-    //   (uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
-    //     this.setState({
-    //       uploadSnapshot
-    //     });
-    //   },
-    //   error => console.log(error),
-    //   () => {
-    //     this.setState({
-    //       uploadSnapshot: undefined
-    //     });
-    //   }
-    // );
   }
-
-  // renderProgress() {
-  //   const { bytesTransferred, totalBytes } = this.state.uploadSnapshot!;
-  //   return <LinearProgress variant="determinate" value={(bytesTransferred / totalBytes) * 100} />;
-  // }
 }
 
-export default ImageUploader;
+export default ImageUploader as React.ComponentType<ImageUploaderProps>;
