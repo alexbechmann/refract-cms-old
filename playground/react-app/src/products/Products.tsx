262ce2419a72c4589047cb1e38e5b17bc03b3956
Alex Bechmann <alex.bechmann@outlook.com>
2018-08-27 08:35:42 +0200

    consumer tweak

--- playground/react-app/src/products/Products.tsx
+++ playground/react-app/src/products/Products.tsx
@@ -52,7 +52,7 @@ class Products extends React.Component<Props, State> {
           Products
         </Typography>
         {this.state.products.map(product => {
-          const crop = product.image.crops && product.image.crops.main ? product.image.crops.main.pixelCrop : undefined;
+          const crop = product.image.crops.main ? product.image.crops.main.pixelCrop : undefined;
           return (
             <Card className={classes.card} key={product._id}>
               {product.image && (
