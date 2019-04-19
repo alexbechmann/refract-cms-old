4f4ec56dd0094301d7b7328cc54ee21207c00f02
Alex Bechmann <alex.bechmann@outlook.com>
2019-04-19 13:21:43 +0200

    remove demo

--- demo/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("*", (req, res) => {
-  res.send("<h1>Hello, world!</h1>");
-});
-
-module.exports = app;
--- demo/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Dashboard } from "@refract-cms/dashboard";
-import config from "./refract-cms/refract.config";
-
-ReactDOM.render(
-  <Dashboard config={config} rootPath="/" serverUrl="/cms" />,
-  document.getElementById("root")
-);
