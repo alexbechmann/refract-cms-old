4951b4169ceb2194854fe2b192c5cd7c1d1b864f
= <=>
2019-04-21 13:47:51 +0200

    dark mode

--- dev/src/client/index.tsx
+++ dev/src/client/index.tsx
@@ -3,8 +3,27 @@ import { render } from 'react-dom';
 import { Dashboard } from '@refract-cms/dashboard';
 import config from '../refract-cms/refract.config';
 import 'typeface-roboto';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import { teal, blue } from '@material-ui/core/colors';
 
-render(<Dashboard config={config} rootPath="/" serverUrl="/cms" />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: blue[500]
+    },
+    secondary: {
+      main: teal[500]
+    }
+  }
+});
+
+render(
+  <MuiThemeProvider theme={theme}>
+    <Dashboard config={config} rootPath="/" serverUrl="/cms" />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept();
