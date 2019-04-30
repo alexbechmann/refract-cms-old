d576facf683fc0f8616b27f9da6452608bc670a9
Alex Bechmann <alex.bechmann@outlook.com>
2019-05-01 00:18:25 +0200

    started work on cli new app

--- packages/cli/bin/refract-cms.js
+++ packages/cli/bin/refract-cms.js
@@ -1,6 +1,11 @@
 #! /usr/bin/env node
 "use strict";
 
+const program = require("commander");
+const fs = require("fs-extra");
+const path = require("path");
+const chalk = require("chalk").default;
+
 if (process.platform === "win32") {
   var rl = require("readline").createInterface({
     input: process.stdin,
@@ -13,8 +18,33 @@ if (process.platform === "win32") {
 }
 
 process.on("SIGINT", function() {
-  //graceful shutdown
   process.exit();
 });
 
-require("../scripts/start");
+program
+  .command("start")
+  .description("start")
+  .action(function() {
+    require("../scripts/start");
+  });
+
+program
+  .command("new [name]")
+  .description("run setup commands for all envs")
+  // .option("-s, --setup_mode [mode]", "Which setup mode to use")
+  .action(function(name, options) {
+    console.log("Creating a new app in " + path.resolve(process.cwd(), name));
+    fs.ensureDirSync(name);
+    fs.copySync(
+      path.join(__dirname, "../new-source-files"),
+      path.join(process.cwd(), name)
+    );
+    console.log(
+      "Created app in " + chalk.blue(path.resolve(process.cwd(), name))
+    );
+    console.log("cd " + name);
+    console.log("npm start");
+    process.exit();
+  });
+
+program.parse(process.argv);
