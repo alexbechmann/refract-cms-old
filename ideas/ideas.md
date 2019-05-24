ff1a6bd7820f7a583e91d394d22bdf5609ee42e3
Alex Bechmann <alex.bechmann@outlook.com>
2019-05-25 00:36:54 +0200

    ideas

--- ideas/ideas.md
+++ ideas/ideas.md
@@ -1,4 +1,4 @@
-```ts
+```tsx
 const BlogItemSchema = defineEntity({
   options: {
     alias: "blobItem",
@@ -10,8 +10,22 @@ const BlogItemSchema = defineEntity({
       editorComponent: createTextEditor({
         maxLength: 50
       }),
+      editorComponent2: props => <TextEditor maxLength={50} {...props} />,
       type: RefractTypes.string
     }
   }
 });
+
+function getPrototype<TPropertyType extends PropertyType>(
+  property: TPropertyType
+) {
+  if (typeof property === "object") {
+    return Object.keys(property).reduce((acc, propertyKey) => {
+      acc[propertyKey] = getPrototype(property[propertyKey]);
+      return acc;
+    }, {});
+  } else {
+    return property.prototype;
+  }
+}
 ```
--- ideas/ideas2.tsx
+++ ideas/ideas2.tsx
@@ -6,6 +6,14 @@ type PropertyType = BasicPropertyType | ShapePropertyType;
 
 type Properties<T> = { [K in keyof T]: PropertyType };
 
+interface PropertyEditorProps<T> {
+  setValue: (value: T | undefined) => void;
+  propertyKey: string;
+  value: T | undefined;
+  propertyOptions: PropertyOptions<T>;
+  serverUrl: string;
+}
+
 // type Properties<T extends {[K in keyof T]: PropertyType}> = { [K in keyof T]: PropertyOptions<T[K]> };
 
 type ActualType<T extends PropertyType> = T extends BasicPropertyType
@@ -14,11 +22,15 @@ type ActualType<T extends PropertyType> = T extends BasicPropertyType
   ? { [K in keyof T]: ActualType<T[K]> }
   : never;
 
-export interface PropertyOptions<TPropertyType extends PropertyType> {
+function createFakeEditor<T>() {
+  return (props: PropertyEditorProps<T>) => null;
+}
+
+export interface PropertyOptions<T> {
   displayName?: string;
-  // editorComponent?: React.ComponentType<PropertyEditorProps<T>>;
-  defaultValue?: ActualType<TPropertyType>;
-  type: TPropertyType;
+  editorComponent?: React.ComponentType<PropertyEditorProps<T>>;
+  defaultValue?: (() => T) | T | Promise<T>;
+  // type: TPropertyType;
 }
 
 type Return<TProperties extends Properties<T>, T> = { [K in keyof TProperties]: ActualType<TProperties[K]> };
@@ -26,6 +38,7 @@ type Return<TProperties extends Properties<T>, T> = { [K in keyof TProperties]: 
 function createSchema<TProperties extends Properties<T>, T>(args: {
   properties: TProperties;
   options: any;
+  editors: { [K in keyof T]: PropertyOptions<ActualType<TProperties[K]>> };
 }) {
   return {
     ...args,
@@ -43,6 +56,31 @@ const ArticleSchema = createSchema({
       lat: Number,
       lng: Number
     }
+  },
+  editors: {
+    name: {
+      editorComponent: createFakeEditor<string>(),
+      defaultValue: '',
+      displayName: ''
+    },
+    createDate: {
+      editorComponent: createFakeEditor<Date>(),
+      defaultValue: () => new Date(),
+      displayName: ''
+    },
+    age: {
+      editorComponent: createFakeEditor<number>(),
+      defaultValue: 4,
+      displayName: ''
+    },
+    location: {
+      editorComponent: createFakeEditor<{ lat: number; lng: number }>(),
+      defaultValue: {
+        lat: 3,
+        lng: 4
+      },
+      displayName: ''
+    }
   }
 });
 
@@ -50,6 +88,11 @@ const CommentSchema = createSchema({
   options: {},
   properties: {
     text: String
+  },
+  editors: {
+    text: {
+      editorComponent: createFakeEditor<string>()
+    }
   }
 });
 
