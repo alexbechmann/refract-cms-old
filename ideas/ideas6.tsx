00e4a88b189a8e873943f51e92e0596f26217cb6
Alex Bechmann <alex.bechmann@outlook.com>
2019-06-02 07:51:07 +0200

    ideas6

--- ideas/ideas6.tsx
+++ ideas/ideas6.tsx
@@ -4,13 +4,11 @@ type ShapePropertyType = { [key: string]: PropertyType };
 
 type PropertyType = BasicPropertyType | ShapePropertyType;
 
-type Properties<T> = { [K in keyof T]: PropertyOptions<PropertyType> };
-
 interface PropertyEditorProps<T> {
   setValue: (value: T | undefined) => void;
   propertyKey: string;
   value: T | undefined;
-  propertyOptions: PropertyOptions<PropertyType>;
+  propertyOptions: PropertyOptions<any, PropertyType>;
   serverUrl: string;
 }
 
@@ -26,16 +24,17 @@ function composeFakeEditor<T>() {
   return (props: PropertyEditorProps<T>) => null;
 }
 
-export interface PropertyOptions<TPropertyType extends PropertyType | any> {
+export interface PropertyOptions<T, TPropertyType extends PropertyType | any> {
   displayName?: string;
   editorComponent: React.ComponentType<PropertyEditorProps<ActualType<TPropertyType>>>;
   defaultValue?: (() => ActualType<TPropertyType>) | ActualType<TPropertyType> | Promise<ActualType<TPropertyType>>;
   type: TPropertyType;
+  resolver?: Resolver<T, ActualType<TPropertyType>>;
 }
 
 interface EntityOptions {}
 
-type Resolver<TProperties, T> = (source: TProperties) => T | Promise<T>;
+type Resolver<T, V> = (source: T) => V | Promise<V>;
 
 // type Return<TProperties extends Properties<T>, T> = { [K in keyof TProperties]: ActualType<TProperties[K]> };
 
@@ -46,17 +45,17 @@ type Resolver<TProperties, T> = (source: TProperties) => T | Promise<T>;
 //   prototypes: ActualType<T>;
 // };
 
-type EntitySchema = ReturnType<typeof composeSchema>;
+type EntitySchema<T = any> = ReturnType<typeof composeSchema>;
 
 function composeSchema<T, TAlias extends string>(args: {
   alias: TAlias;
-  properties: { [K in keyof T]: PropertyOptions<T[K]> };
+  properties: { [K in keyof T]: PropertyOptions<T, T[K]> };
   options: EntityOptions;
   // editors: { [K in keyof TProperties]: PropertyOptions<ActualType<TProperties[K]>> };
   // resolvers: { [K in keyof Partial<TProperties>]: Resolver<ActualType<TProperties>, ActualType<TProperties[K]>> };
 }) {
   // EntitySchema<T, TAlias>
-  type F = { [K in keyof T]: PropertyOptions<T[K]> };
+  type F = { [K in keyof T]: PropertyOptions<T, T[K]> };
   type Return = { [K in keyof F]: ActualType<F[K]['type']> };
   return {
     ...args,
@@ -80,6 +79,7 @@ const CommentSchema = composeSchema({
       editorComponent: composeFakeEditor<string>(),
       defaultValue: 'hello',
       displayName: 'Text'
+      //resolve: (s) => ''
     },
     location: {
       editorComponent: composeFakeEditor<{ lat: number; lng: number }>(),
@@ -103,31 +103,7 @@ const ArticleSchema = composeSchema({
       displayName: 'Text'
     }
   }
-  // editors: {
-  //   title: {
-  //     editorComponent: composeFakeEditor<string>()
-  //   }
-  // },
-  // resolvers: {
-  //   title: article => article.title + 'extra'
-  // }
 });
-
-// composeEditors(CommentSchema, {
-//   text: {
-//     editorComponent: composeFakeEditor<string>(),
-//     defaultValue: 'hello',
-//     displayName: 'Text'
-//   },
-//   publishDate: {
-//     editorComponent: composeFakeEditor<Date>(),
-//     displayName: 'Text'
-//   },
-//   location: {
-//     editorComponent: composeFakeEditor<{ lat: number; lng: number }>(),
-//     displayName: 'Text'
-//   }
-// });
 
 interface Config<TEntitySchemas extends EntitySchema[] = []> {
   schemas: TEntitySchemas;
@@ -136,50 +112,9 @@ interface Config<TEntitySchemas extends EntitySchema[] = []> {
 function configure<TEntitySchemas extends EntitySchema[]>(config: Config<TEntitySchemas>) {
   return config;
 }
-var s = {
-  [CommentSchema.alias]: CommentSchema,
-  [ArticleSchema.alias]: ArticleSchema
-};
-
-var [a, b] = [CommentSchema, ArticleSchema];
-a.alias = 'd';
-
 const config = configure({
   schemas: [CommentSchema, ArticleSchema]
 });
 
-// type AliasFromEntitySchema<TEntitySchema extends EntitySchema> = TEntitySchema['alias'];
-
-// type D1<TProperties extends Properties<T>, T, E extends EntitySchema<TProperties>[]> = E extends (infer U)[]
-//   ? E[U]
-//   : never;
-
-function configureClient<TEntitySchemas extends EntitySchema[]>(
-  config: Config<TEntitySchemas>
-  //clientConfig: { [K in keyof TEntitySchemas[number]['alias']]: any }
-): { [K in keyof TEntitySchemas[number]['alias']]: TEntitySchemas[K] } {
-  return {} as any;
-}
-
-ArticleSchema.alias;
-
-var clientConfig = configureClient(config);
-
-clientConfig[0].alias;
-
 CommentSchema.prototypes.text.toUpperCase();
 CommentSchema.prototypes.location.lat.toFixed();
-
-function asSchemaType<T, TProperties extends Properties<T>>(schema: EntitySchema<TProperties, T>, obj: any) {
-  return obj as ActualType<TProperties>;
-}
-
-type TypeFromSchema<TEntitySchema extends EntitySchema> = ActualType<TEntitySchema['properties']>;
-
-var comment = asSchemaType(CommentSchema, {});
-
-comment.text.toLocaleLowerCase();
-
-var comment2: TypeFromSchema<typeof CommentSchema> = {} as any;
-comment2.text;
-comment.location.lat;
