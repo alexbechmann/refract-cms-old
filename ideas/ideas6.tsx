e2aaca4f5489d330cf24f86e1296103b7e49346f
Alex Bechmann <alex.bechmann@outlook.com>
2019-06-03 10:03:20 +0200

    resolver ideas6

--- ideas/ideas6.tsx
+++ ideas/ideas6.tsx
@@ -26,15 +26,17 @@ function composeFakeEditor<T>() {
 
 export interface PropertyOptions<T, TPropertyType extends PropertyType | any> {
   displayName?: string;
-  editorComponent: React.ComponentType<PropertyEditorProps<ActualType<TPropertyType>>>;
+  editorComponent?: React.ComponentType<PropertyEditorProps<ActualType<TPropertyType>>>;
   defaultValue?: (() => ActualType<TPropertyType>) | ActualType<TPropertyType> | Promise<ActualType<TPropertyType>>;
   type: TPropertyType;
-  resolver?: Resolver<T, ActualType<TPropertyType>>;
+  resolve?: Resolver<T, ActualType<TPropertyType>>;
 }
 
-interface EntityOptions {}
+interface EntityOptions {
+  alias: string;
+}
 
-type Resolver<T, V> = (source: T) => V | Promise<V>;
+type Resolver<T, V> = (source: { [K in keyof T]: ActualType<T[K]> }) => V | Promise<V>;
 
 // type Return<TProperties extends Properties<T>, T> = { [K in keyof TProperties]: ActualType<TProperties[K]> };
 
@@ -47,16 +49,15 @@ type Resolver<T, V> = (source: T) => V | Promise<V>;
 
 type EntitySchema<T = any> = ReturnType<typeof composeSchema>;
 
-function composeSchema<T, TAlias extends string>(args: {
-  alias: TAlias;
+function composeSchema<T>(args: {
   properties: { [K in keyof T]: PropertyOptions<T, T[K]> };
   options: EntityOptions;
   // editors: { [K in keyof TProperties]: PropertyOptions<ActualType<TProperties[K]>> };
   // resolvers: { [K in keyof Partial<TProperties>]: Resolver<ActualType<TProperties>, ActualType<TProperties[K]>> };
 }) {
   // EntitySchema<T, TAlias>
-  type F = { [K in keyof T]: PropertyOptions<T, T[K]> };
-  type Return = { [K in keyof F]: ActualType<F[K]['type']> };
+  type ObjectWithConstructorTypes = { [K in keyof T]: PropertyOptions<T, T[K]> };
+  type Return = { [K in keyof ObjectWithConstructorTypes]: ActualType<ObjectWithConstructorTypes[K]['type']> };
   return {
     ...args,
     prototypes: {} as Return
@@ -71,15 +72,13 @@ function composeSchema<T, TAlias extends string>(args: {
 // }
 
 const CommentSchema = composeSchema({
-  alias: 'comment',
-  options: {},
+  options: { alias: 'comment' },
   properties: {
     text: {
       type: String,
       editorComponent: composeFakeEditor<string>(),
       defaultValue: 'hello',
       displayName: 'Text'
-      //resolve: (s) => ''
     },
     location: {
       editorComponent: composeFakeEditor<{ lat: number; lng: number }>(),
@@ -88,13 +87,16 @@ const CommentSchema = composeSchema({
         lat: Number,
         lng: Number
       }
+    },
+    upperCaseText: {
+      type: String,
+      resolve: comment => comment.text.toUpperCase()
     }
   }
 });
 
 const ArticleSchema = composeSchema({
-  alias: 'article',
-  options: {},
+  options: { alias: 'article' },
   properties: {
     title: {
       type: String,
