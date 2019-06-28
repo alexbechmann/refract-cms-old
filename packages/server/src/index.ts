e045d0b74f040d6c3685f368b42af401573e3be8
Alex Bechmann <alex.bechmann@danfoss.com>
2019-06-28 11:34:07 +0200

    export context

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,3 +3,4 @@ export { repositoryForSchema } from './repository-for-schema';
 export { ServerConfig } from './server-config.model';
 export { createResolver } from './graphql/create-resolver';
 export { PublicSchemaBuilder } from './graphql/public-schema.builder';
+export { RefractGraphQLContext } from './graphql/refract-graphql-context';
